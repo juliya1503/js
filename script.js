90af18f43225c3b56d77cb01203c746e806db5af
julia <julia@gmail.com>
2021-01-15 23:31:33 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var names = prompt(' Ваше Имя ');
+console.log(names);
+
+var primer = ('Решите примеры')
+alert(primer);
+var num1 = +prompt(' 5 + 6 = ?');
+console.log(num1);
+
+var num2 = +prompt(' 5 * 6 = ?');
+console.log(num2);
+
+var num3 = +prompt(' 6 / 5 = ?');
+console.log(num3);
+
+
+var num4 = +prompt(' ( 5 + 6 ) * 10 = ?');
+console.log(num4);
+
+var num5 = +prompt(' ( 10 + 12 ) / ( 11 + 2 ) = ?');
+console.log(num5);
+
+var allInfo = ( ' Привет ' + names +  '. Посмотрим ваши ответы. ' + ' Ваш ответ на первый пример: ' + num1 + '. Правильный ответ: 11 ' + '. Ваш ответ на второй пример: ' + num2 + '. Правильный ответ: 30 ' + '. Ваш ответ на третий пример: ' + num3 + '. Правильный ответ: 1,2 ' + '. Ваш ответ на четвертый пример: ' + num4 + '. Правильный ответ: 110 ' + '. Ваш ответ на пятый пример: ' + num5 + '. Правильный ответ: 1 ');
+alert(allInfo);
+console.log(allInfo);
+
+
+
+
+
+
+
